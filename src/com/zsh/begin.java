fbdbe84442e89566281852a8d608c7171bdf3ece
zzzsh123 <57971494+zzzsh123@users.noreply.github.com>
2020-05-27 15:22:18 +0800

    流式编程加上Lambda表达式输出Hello JAVA8

--- src/com/zsh/begin.java
+++ src/com/zsh/begin.java
@@ -1,4 +1,11 @@
 package com.zsh;
 
+import java.util.stream.Stream;
+
 public class begin {
+
+    public static void main(String[] args) {
+        Stream.of("Hello JAVA8").forEach(System.out::println);
+    }
+
 }
